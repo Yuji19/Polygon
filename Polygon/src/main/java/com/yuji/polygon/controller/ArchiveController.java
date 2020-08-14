62cf339114b3eafcc19d013e40f41a6e9d1e3f99
chenyuji <201998@gree.com.cn>
2020-08-14 17:21:02 +0800

    ArchiveServiceImpl中增加insertArchive方法

--- Polygon/src/main/java/com/yuji/polygon/controller/ArchiveController.java
+++ Polygon/src/main/java/com/yuji/polygon/controller/ArchiveController.java
@@ -2,9 +2,16 @@ package com.yuji.polygon.controller;
 
 
 import com.yuji.polygon.entity.APIException;
+import com.yuji.polygon.entity.Archive;
+import com.yuji.polygon.service.impl.ArchiveServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.constraints.NotNull;
 
 /**
  * @className: ArchiveController
@@ -17,11 +24,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/archive")
 public class ArchiveController {
 
+    @Autowired
+    ArchiveServiceImpl archiveService;
+
     @GetMapping("/all")
     public String listArchives(Integer currentPageNum, Integer pageSize){
         //Archive archive = new Archive();
         //archive.setFileName("ceshi");
         //return archive;
         throw new APIException("测试抛出异常");
+    }
+
+    @PostMapping("/add")
+    public String insert(@NotNull Archive archive,@NotNull MultipartFile file){
+        return archiveService.insertArchive(archive, file);
     }
 }
--- Polygon/src/main/java/com/yuji/polygon/service/impl/ArchiveServiceImpl.java
+++ Polygon/src/main/java/com/yuji/polygon/service/impl/ArchiveServiceImpl.java
@@ -1,7 +1,15 @@
 package com.yuji.polygon.service.impl;
 
+import com.yuji.polygon.entity.APIException;
+import com.yuji.polygon.entity.Archive;
+import com.yuji.polygon.entity.ResultCode;
+import com.yuji.polygon.mapper.ArchiveMapper;
 import com.yuji.polygon.service.ArchiveService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
 
 /**
  * @className: ArchiveServiceImpl
@@ -12,4 +20,20 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ArchiveServiceImpl implements ArchiveService {
+
+    @Autowired
+    ArchiveMapper archiveMapper;
+
+    public String insertArchive(Archive archive, MultipartFile file){
+        Archive oldArchive = archiveMapper.findArchiveByFileNo(archive.getFileNo());
+        Optional.ofNullable(oldArchive)
+                .orElseThrow(() -> new APIException("文档编号已存在"));
+
+        if(Optional.ofNullable(archiveMapper.insertArchive(archive)).isPresent()){
+            return "添加文档成功";
+        }else {
+            throw new APIException("添加文档失败");
+        }
+
+    }
 }
